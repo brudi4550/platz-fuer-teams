e9a09508a29982bfd7f9065327309736aac3f3b1
Alexander Wolf <alexanderwolf@outlook.at>
2021-11-03 19:28:59 +0100

    initial commit

---
+++ platz-fuer-teams/MainScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreen.swift
+//  platz-fuer-teams
+//
+//  Created by Alexander Wolf on 30.10.21.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text("This is the main screen")
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
---
+++ platz-fuer-teams/platz_fuer_teamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  platz_fuer_teamsApp.swift
+//  platz-fuer-teams
+//
+//  Created by Alexander Wolf on 30.10.21.
+//
+
+import SwiftUI
+
+@main
+struct platz_fuer_teamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
